a706d4ddab48781b28f3f82231dcae827ff76bca
jasmineaurely <aurelysj@gmail.com>
2021-01-05 08:43:45 +0700

    tugas wad

--- esport-resonance-main/Modules/Match/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('match::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('match.name') !!}
-    </p>
-@endsection
